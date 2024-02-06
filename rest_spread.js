ff07a393e5831f97f0c0d8beb48b75011f670501
Clay O'Dell <clay.odell.music@gmail.com>
2024-02-06 12:51:18 -0600

    exercises all the way to Slice and Dice 1st function

---
+++ rest_spread.js
@@ -0,0 +1,25 @@
+const filterOutOdds = (...nums) => {
+    return nums.filter((num) => num % 2 === 0);
+}
+
+
+function findMin(...nums){
+    return Math.min(...nums);
+}
+const restAnswer2 = findMin(1,4,12,-3)
+const restAnswer3 = findMin(1,-1)
+const restAnswer4 = findMin(3,1)
+
+const mergeObjects = (a,b) => ({...a,...b });
+
+const restAnswer5 = mergeObjects({a:1, b:2}, {c:3, d:4})
+
+const doubleAndReturnArgs = (arr, ...args) => [...arr, ...args.map(val => val * 2)]
+
+const restAnswer6 = doubleAndReturnArgs([1,2,3],4,4);
+const restAnswer7 = doubleAndReturnArgs([2],10,4);
+
+const removeRandom = items => {
+    let index = Math.floor(Math.random() * items.length);
+    return [...items.slice(0, index), ...items.slice(index + 1)];
+}
